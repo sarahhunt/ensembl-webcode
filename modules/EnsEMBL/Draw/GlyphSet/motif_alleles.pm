9191b85d2a38a0b42996df51a10d4cce41354226
Anne Lyle <annelyle@ebi.ac.uk>
2015-03-23 11:27:01 +0000

    New glyphset for showing variations on a sequence logo

---
+++ modules/EnsEMBL/Draw/GlyphSet/motif_alleles.pm
@@ -0,0 +1,35 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Draw::GlyphSet::motif_alleles;
+
+### Draws a SNP track on a sequence logo
+
+use strict;
+
+use base qw(EnsEMBL::Draw::GlyphSet::_variation);
+
+sub depth { return 4; }
+
+sub feature_label { 
+  my ($self, $feature) = @_;
+  my $label = $feature->minor_allele;
+  return $label unless $label eq '-'; 
+}
+
+1;
