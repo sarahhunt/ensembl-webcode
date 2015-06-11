e6c5134b641b177ef334da7f74beb248bef1fb0b
Anne Lyle <annelyle@ebi.ac.uk>
2015-06-11 12:55:20 +0100

    Fixed package name

--- modules/EnsEMBL/Web/Role/Bio/Gene/Homology.pm
+++ modules/EnsEMBL/Web/Role/Bio/Gene/Homology.pm
@@ -16,7 +16,7 @@ limitations under the License.
 
 =cut
 
-package EnsEMBL::Web::Role::Bio::Gene::Compara;
+package EnsEMBL::Web::Role::Bio::Gene::Homology;
 
 ### Compara-specific data-munging for gene pages
 
